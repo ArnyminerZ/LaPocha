39c7dae67e55cd0b3f1da98fd76f0c23eba4592d
Arnau Mora <arnyminer.z@gmail.com>
2022-08-21 04:05:21 +0200

    :sparkles: Added fonts
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/games/la_pocha/ui/fonts/Barlow.kt
@@ -0,0 +1,33 @@
+package com.arnyminerz.games.la_pocha.ui.fonts
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.arnyminerz.games.la_pocha.R
+
+/**
+ * The Barlow Font Family.
+ * @author Google Fonts
+ * @see <a href="https://fonts.google.com/specimen/Barlow?selected=Material+Icons&category=Serif,Sans+Serif">Google Fonts</a>
+ */
+val barlowFamily = FontFamily(
+    Font(R.font.barlow_black, FontWeight.Black),
+    Font(R.font.barlow_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.barlow_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.barlow_extra_bold_italic, FontWeight.ExtraBold, FontStyle.Italic),
+    Font(R.font.barlow_bold, FontWeight.Bold),
+    Font(R.font.barlow_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.barlow_semi_bold, FontWeight.SemiBold),
+    Font(R.font.barlow_semi_bold_italic, FontWeight.SemiBold, FontStyle.Italic),
+    Font(R.font.barlow_medium, FontWeight.Medium),
+    Font(R.font.barlow_medium_italic, FontWeight.Medium, FontStyle.Italic),
+    Font(R.font.barlow_regular, FontWeight.Normal),
+    Font(R.font.barlow_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.barlow_thin, FontWeight.Thin),
+    Font(R.font.barlow_thin_italic, FontWeight.Thin, FontStyle.Italic),
+    Font(R.font.barlow_light, FontWeight.Light),
+    Font(R.font.barlow_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.barlow_extra_light, FontWeight.ExtraLight),
+    Font(R.font.barlow_extra_light_italic, FontWeight.ExtraLight, FontStyle.Italic),
+)
---
+++ app/src/main/java/com/arnyminerz/games/la_pocha/ui/fonts/RobotoSlab.kt
@@ -0,0 +1,23 @@
+package com.arnyminerz.games.la_pocha.ui.fonts
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.arnyminerz.games.la_pocha.R
+
+/**
+ * The Roboto Slab Font Family.
+ * @author Google Fonts
+ * @see <a href="https://fonts.google.com/specimen/Roboto+Slab?selected=Material+Icons&category=Serif,Sans+Serif">Google Fonts</a>
+ */
+val robotoSlabFamily = FontFamily(
+    Font(R.font.roboto_slab_black, FontWeight.Black),
+    Font(R.font.roboto_slab_extra_bold, FontWeight.ExtraBold),
+    Font(R.font.roboto_slab_bold, FontWeight.Bold),
+    Font(R.font.roboto_slab_semi_bold, FontWeight.SemiBold),
+    Font(R.font.roboto_slab_medium, FontWeight.Medium),
+    Font(R.font.roboto_slab_regular, FontWeight.Normal),
+    Font(R.font.roboto_slab_thin, FontWeight.Thin),
+    Font(R.font.roboto_slab_light, FontWeight.Light),
+    Font(R.font.roboto_slab_extra_light, FontWeight.ExtraLight),
+)
