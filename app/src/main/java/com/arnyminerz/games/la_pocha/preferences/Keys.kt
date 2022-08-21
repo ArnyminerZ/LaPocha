9fd744c31e61d65d2e1a50e5aeb31b865b8693c8
Arnau Mora <arnyminer.z@gmail.com>
2022-08-21 13:01:18 +0200

    :sparkles: Added `GAME_INFO`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/games/la_pocha/preferences/Keys.kt
+++ app/src/main/java/com/arnyminerz/games/la_pocha/preferences/Keys.kt
@@ -1,4 +1,4 @@
 package com.arnyminerz.games.la_pocha.preferences
 
 const val PREFERENCES_KEY_SHOWN_INTRO = "shown_intro"
-const val PREFERENCES_KEY_GAME_PLAYERS = "game_info_players"
+const val PREFERENCES_KEY_GAME_INFO = "game_info"
--- app/src/main/java/com/arnyminerz/games/la_pocha/preferences/Preferences.kt
+++ app/src/main/java/com/arnyminerz/games/la_pocha/preferences/Preferences.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.stringSetPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.arnyminerz.games.la_pocha.BuildConfig
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = BuildConfig.APPLICATION_ID)
 
 val SHOWN_INTRO = booleanPreferencesKey(PREFERENCES_KEY_SHOWN_INTRO)
-val GAME_INFO_PLAYERS = stringSetPreferencesKey(PREFERENCES_KEY_GAME_PLAYERS)
+
+val GAME_INFO = stringPreferencesKey(PREFERENCES_KEY_GAME_INFO)
